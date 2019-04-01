eb17419fda14402237ef4a7a87c1af772a4973a3
Damien Ayers <damien@omad.net>
2019-04-01 13:07:05 +1100

    Fix URL generation on Windows
    Always use the `posixpath` module for joining URL components.

--- stac_validator/stac_utilities.py
+++ stac_validator/stac_utilities.py
@@ -4,7 +4,7 @@ Description: Utilities for STAC validate
 """
 __author__ = "James Banting"
 
-import os
+import posixpath
 
 import requests
 
@@ -38,19 +38,19 @@ class StacVersion:
 
         # Collection spec can be validated by catalog spec.
         if requests.get(cdn_base_url).status_code == 200:
-            self.ITEM_URL = os.path.join(cdn_base_url, self.filename)
-            self.CATALOG_URL = os.path.join(cdn_base_url, self.filename)
-            self.COLLECTION_URL = os.path.join(cdn_base_url, self.filename)
+            self.ITEM_URL = posixpath.join(cdn_base_url, self.filename)
+            self.CATALOG_URL = posixpath.join(cdn_base_url, self.filename)
+            self.COLLECTION_URL = posixpath.join(cdn_base_url, self.filename)
         else:
             if self.version in old_versions:
-                self.CATALOG_URL = os.path.join(git_base_url, f"static-catalog/{self.input_type}/{self.filename}")
-                self.ITEM_URL = os.path.join(git_base_url, f"json-spec/{self.input_type}/{self.filename}")
+                self.CATALOG_URL = posixpath.join(git_base_url, f"static-catalog/{self.input_type}/{self.filename}")
+                self.ITEM_URL = posixpath.join(git_base_url, f"json-spec/{self.input_type}/{self.filename}")
 
             else:
 
-                self.CATALOG_URL = os.path.join(git_base_url, f"catalog-spec/{self.input_type}/{self.filename}")
-                self.COLLECTION_URL = os.path.join(git_base_url, f"collection-spec/{self.input_type}/{self.filename}")
-                self.ITEM_URL = os.path.join(git_base_url, f"item-spec/{self.input_type}/{self.filename}")
+                self.CATALOG_URL = posixpath.join(git_base_url, f"catalog-spec/{self.input_type}/{self.filename}")
+                self.COLLECTION_URL = posixpath.join(git_base_url, f"collection-spec/{self.input_type}/{self.filename}")
+                self.ITEM_URL = posixpath.join(git_base_url, f"item-spec/{self.input_type}/{self.filename}")
 
     @staticmethod
     def fix_stac_item(version, filename):
